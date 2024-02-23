1b24e692ed67a48c6a656d4f1911f2188bf24255
KourifaZineb <zinebkourifa@gmail.com>
2024-02-23 16:47:22 +0100

    V3

--- src/main/java/pres/presAvecSpringXML.java
+++ src/main/java/pres/presAvecSpringXML.java
@@ -1,2 +1,14 @@
-package pres;public class presAvecSpringXML {
+package pres;
+
+import dao.IDao;
+import metier.IMetier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class presAvecSpringXML {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        IMetier metier = context.getBean(IMetier.class);
+        System.out.println("Result = " + metier.calcul());
+    }
 }
